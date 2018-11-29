8b17275baf425430d545a2fb47025de7083cb64d
Giannis <bone.jp@gmail.com>
2018-11-29 21:13:09 +0200

    Update dropdown plugin

--- dropdown.js
+++ dropdown.js
@@ -1,29 +1,41 @@
 /*!
  * Version: 1.0
  * Started: 11-06-2013
- * Updated: 11-09-2018
+ * Updated: 11-04-2015
  * Author : paramana (hello AT paramana DOT com)
  *
  */
-define(['jquery'], function($) {
-    'use strict';
 
-    var pluginName = 'dropdown',
+define([
+    'jquery'
+], function($) {
+    // Using ECMAScript 5 strict mode during development. By default r.js will ignore that.
+    "use strict";
+
+    // Create the defaults once
+    var pluginName = "dropdown",
         defaults   = {
-            activeClass: 'active',
-            activeSelect: 'active-select',
-            errorClass: 'error',
-            selectedClass:  'selected',
+            activeClass: "active",
+            activeSelect: "active-select",
+            errorClass: "error",
+            selectedClass:  "selected",
             rel: this,
             onchange: function(){}
         };
 
+    // The actual plugin constructordropdown-element
     function Dropdown(element, options) {
         this.element = element;
         this.$element = $(element);
         this.$select = this.$element.find('select');
         this.$text = this.$element.find('span:eq(0)')
         this.enabled = true;
+
+        // jQuery has an extend method that merges the
+        // contents of two or more objects, storing the
+        // result in the first object. The first object
+        // is generally empty because we don't want to alter
+        // the default options for future instances of the plugin
         this.options = $.extend({}, defaults, options);
 
         this._defaults = defaults;
@@ -33,15 +45,11 @@ define(['jquery'], function($) {
     }
 
     Dropdown.prototype.init = function() {
-        var $selected;
-
-        if (this.$element.hasClass('dropdown-element'))
-            return;
-
         this.$element.addClass('dropdown-element');
         this.events();
 
-        if (this.options.value !== false || this.options.value !== '')
+        var $selected;
+        if (this.options.value)
             $selected = this.$select.find('option[value="' + this.options.value + '"]').attr('selected', 'selected');
         else
             $selected = this.$select.find('option[selected="selected"]');
@@ -63,8 +71,8 @@ define(['jquery'], function($) {
         }
 
         this.$element
-            .removeClass(this.activeSelect + ' ' + this.error)
-            .data('value', _value);
+                .removeClass(this.activeSelect + ' ' + this.error)
+                .data('value', _value);
 
         this.$text.text($option.text());
 
@@ -87,11 +95,11 @@ define(['jquery'], function($) {
             change: function(){
                 _self.setValue();
             },
-            click: function(event) {
+            click: function(e) {
                 if (!_self.enabled)
                     return false;
 
-                event.stopPropagation()
+                // e.stopPropagation()
             },
             keyup: function() {
                 if (!_self.enabled)
@@ -99,13 +107,13 @@ define(['jquery'], function($) {
 
                 _self.setValue();
             },
-            focus: function() {
+            focus: function(event) {
                 if (!_self.enabled)
                     return false;
 
                 _self.$element.addClass(_self.options.activeClass);
             },
-            blur: function() {
+            blur: function(event) {
                 if (!_self.enabled)
                     return false;
 
@@ -126,10 +134,15 @@ define(['jquery'], function($) {
         this.enabled = false;
     }
 
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
     $.fn[pluginName] = function(options) {
         return this.each(function() {
-            if (!$.data(this, 'plugin_' + pluginName))
-                $.data(this, 'plugin_' + pluginName, new Dropdown(this, options));
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName,
+                        new Dropdown(this, options));
+            }
         });
     }
 });
