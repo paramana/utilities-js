8ce0f3dc5ec6f7aa85c5f5bf932699162017c89a
Giannis <bone.jp@gmail.com>
2019-01-30 11:26:43 +0200

    Eslint checkbox

--- checkbox.js
+++ checkbox.js
@@ -1,44 +1,34 @@
 /*!
  * Version: 1.0
  * Started: 07-02-2014
- * Updated: 11-04-2015
+ * Updated: 30-01-2019
  * Author : paramana (hello AT paramana DOT com)
  *
  */
 
-define([
-    'jquery'
-], function($) {
-    // Using ECMAScript 5 strict mode during development. By default r.js will ignore that.
-    "use strict";
+define(['jquery'], function($) {
+    'use strict';
 
-    // Create the defaults once
-    var pluginName = "checkbox",
+    var pluginName = 'checkbox',
         defaults   = {
-            activeClass: "active",
-            checkedClass:  "checked",
+            activeClass: 'active',
+            checkedClass:  'checked',
             rel: this,
             onchange: function(){}
         };
 
-    // The actual plugin constructordropdown-element
     function CheckboxBtn(element, options) {
         this.element       = element;
         this.$element      = $(element);
         this.$checkbox     = this.$element.find('input:checkbox');
         this.originalValue = this.$checkbox.val();
         this.enabled       = true;
-        
-        // jQuery has an extend method that merges the 
-        // contents of two or more objects, storing the 
-        // result in the first object. The first object 
-        // is generally empty because we don't want to alter 
-        // the default options for future instances of the plugin
+
         this.options = $.extend({}, defaults, options);
-        
+
         this.context = this.options.rel;
         this.$inputs = this.context.find('input[name="' + this.$checkbox.attr('name') + '"]');
-        
+
         this._defaults = defaults;
         this._name = pluginName;
 
@@ -62,7 +52,7 @@ define([
             this.$checkbox.trigger('change');
         }
     };
-    
+
     CheckboxBtn.prototype.toggleCheck = function() {
         if (!this.enabled)
             return false;
@@ -72,13 +62,13 @@ define([
 
         if (checked) {
             this.$checkbox
-                    .val(_value)
-                    .addClass(this.options.checkedClass)
-                    .data('text', this.$element.find('.checkbox-text:eq(0)').text());
+                .val(_value)
+                .addClass(this.options.checkedClass)
+                .data('text', this.$element.find('.checkbox-text:eq(0)').text());
 
             this.$element
-                    .addClass(this.options.checkedClass)
-                    .data('value', _value);
+                .addClass(this.options.checkedClass)
+                .data('value', _value);
         }
         else {
             this.$checkbox.removeClass(this.options.checkedClass);
@@ -91,66 +81,63 @@ define([
         var _self = this;
 
         this.$checkbox.on({
-            change: function(e) {
+            change: function() {
                 _self.toggleCheck();
             },
-            click: function(e) {
+            click: function(event) {
                 if (!_self.enabled)
                     return false;
-                
-                e.stopPropagation();
+
+                event.stopPropagation();
             },
             keyup: function() {
                 if (!_self.enabled)
                     return false;
-                
+
                 _self.toggleCheck();
             },
-            focus: function(event) {
+            focus: function() {
                 if (!_self.enabled)
                     return false;
-                
+
                 _self.$element.addClass(_self.options.activeClass);
             },
-            blur: function(event) {
+            blur: function() {
                 if (!_self.enabled)
                     return false;
-                
+
                 _self.$element.removeClass(_self.options.activeClass);
             }
         });
-        
+
         this.$element.on({
-           click: function(){
+            click: function(){
                 var checked = _self.$checkbox.prop('checked');
 
                 _self.$checkbox
-                       .prop('checked', checked ? '' : 'checked')
-                       .trigger('change');
-           } 
+                    .prop('checked', checked ? '' : 'checked')
+                    .trigger('change');
+            }
         });
     };
-    
+
     CheckboxBtn.prototype.enable = function() {
         this.$element.removeClass('disabled');
         this.$checkbox.removeAttr('disabled');
         this.enabled = true;
     };
-    
+
     CheckboxBtn.prototype.disable = function() {
         this.$element.addClass('disabled');
         this.$checkbox.attr('disabled', 'disabled');
         this.enabled = false;
     };
-    
 
-    // A really lightweight plugin wrapper around the constructor, 
-    // preventing against multiple instantiations
     $.fn[pluginName] = function(options) {
         return this.each(function() {
-            if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName,
-                        new CheckboxBtn(this, options));
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName,
+                    new CheckboxBtn(this, options));
             }
         });
     };
